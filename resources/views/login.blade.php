1055fbe47ebf5c4a1c68a7542ffcf119276253a0
sopyandi <sopyandirian12@gmail.com>
2022-08-23 11:45:56 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <center>
+    <div class="box">
+        <div class="foto">
+            <p></p>
+        </div>
+        <br>
+        <form action="">
+        <input type="text" placeholder="Username"><br><br>
+        <input type="password" placeholder="password"><br><br>
+        <input type="submit" value="LOGIN" id="login">
+        </form>
+    </div>
+    </center>
+</body>
+</html>
